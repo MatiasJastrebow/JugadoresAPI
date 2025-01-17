c6412f423e0e02a79681d08a1d08f80497fa0eba
Matías Jastrebow <mjastrebow@frba.utn.edu.ar>
2025-01-17 12:16:37 -0300

    uso memoryCache

--- Program.cs
+++ Program.cs
@@ -3,6 +3,7 @@ using PruebaAPI.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
+builder.Services.AddMemoryCache();
 builder.Services.AddScoped<IJugadoresService, JugadoresService>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- Services/JugadoresService.cs
+++ Services/JugadoresService.cs
@@ -2,17 +2,40 @@
 using PruebaAPI.DTOs;
 using PruebaAPI.Interfaces.ServicesInterfaces;
 using PruebaAPI.Interfaces.ModelsInterfaces;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace PruebaAPI.Services
 {
     public class JugadoresService : IJugadoresService
     {
-        private static long lastId = 0; 
-        private readonly List<IJugador> Jugadores;
+
+        private readonly IMemoryCache _memoryCache;
+        private long lastId;
+        private readonly List<IJugador> jugadores;
+
+        public JugadoresService(IMemoryCache memoryCache)
+        {
+            _memoryCache = memoryCache;
+            if (!_memoryCache.TryGetValue("jugadores", out List<IJugador> _jugadores))
+            {
+                _jugadores = new List<IJugador>
+                {
+                    new Jugador(0, new DateOnly(2004, 5, 18), "Matias", 3)
+                };
+                _memoryCache.Set("jugadores", _jugadores);
+            }
+            if (!_memoryCache.TryGetValue("lastId", out long _lastId))
+            {
+                _lastId = 0; 
+                _memoryCache.Set("lastId", _lastId);
+            }
+            this.jugadores = _memoryCache.Get<List<IJugador>>("jugadores");
+            this.lastId = _memoryCache.Get<long>("lastId");
+        }
 
         public JugadoresService()
         {
-            this.Jugadores = new List<IJugador>
+            this.jugadores = new List<IJugador>
             {
                 new Jugador (0, new DateOnly (2004, 5, 18), "Matias", 3)
             };
@@ -21,56 +44,56 @@ namespace PruebaAPI.Services
         //GET
         public IList<IJugador> GetAll()
         {
-            return this.Jugadores;
+            return this.jugadores;
         }
 
         public IJugador GetById(long id)
         {
-            return this.Jugadores.FirstOrDefault(j => j.id == id);
+            return this.jugadores.FirstOrDefault(j => j.id == id);
         }
 
         public IList<IJugador> GetMayoresA(int edad)
         {
-            return this.Jugadores.FindAll(j => j.GetEdad() > edad);
+            return this.jugadores.FindAll(j => j.GetEdad() > edad);
         }
 
         public IList<IJugador> GetMayoresAFor(int edad)
         {
             List<IJugador> lista = new();
-            for (int i = 0; i < Jugadores.Count; i++)
+            for (int i = 0; i < jugadores.Count; i++)
             {
-                if (Jugadores[i].GetEdad() > edad) lista.Add(Jugadores[i]);
+                if (jugadores[i].GetEdad() > edad) lista.Add(jugadores[i]);
             }
             return lista;
         }
 
         public IList<String> GetJugadoresConMasExperienciaA(int anios)
         {
-            List<IJugador> lista = this.Jugadores.FindAll(j => j.experienceInYears > anios);
+            List<IJugador> lista = this.jugadores.FindAll(j => j.experienceInYears > anios);
             return lista.ConvertAll(j => j.name);
         }
 
         public IList<String> GetJugadoresConMasExperienciaAFor(int anios)
         {
             List<IJugador> listaJugadores = new();
-            for (int i = 0; i < Jugadores.Count; i++)
+            for (int i = 0; i < jugadores.Count; i++)
             {
-                if (Jugadores[i].experienceInYears > anios) listaJugadores.Add(Jugadores[i]);
+                if (jugadores[i].experienceInYears > anios) listaJugadores.Add(jugadores[i]);
             }
             return this.ObtenerNombres(listaJugadores);
         }
 
         public IList<IJugador> GetJugadoresCuyoNombreContieneA(string cadena)
         {
-            return this.Jugadores.FindAll(j => j.name.Contains(cadena) && j.GetEdad() < 25 && j.GetEdad() > 18);
+            return this.jugadores.FindAll(j => j.name.Contains(cadena) && j.GetEdad() < 25 && j.GetEdad() > 18);
         }
 
         public IList<IJugador> GetJugadoresCuyoNombreContieneAFor(string cadena)
         {
             List<IJugador> lista = new();
-            for (int i = 0; i < Jugadores.Count; i++)
+            for (int i = 0; i < jugadores.Count; i++)
             {
-                if (Jugadores[i].GetEdad() > 18 && Jugadores[i].GetEdad() < 25 && Jugadores[i].NombreContieneCadena(cadena)) lista.Add(Jugadores[i]);
+                if (jugadores[i].GetEdad() > 18 && jugadores[i].GetEdad() < 25 && jugadores[i].NombreContieneCadena(cadena)) lista.Add(jugadores[i]);
             }
             return lista;
         }
@@ -81,7 +104,8 @@ namespace PruebaAPI.Services
             if (jugadorDto.birthDate is not null && jugadorDto.name is not null && jugadorDto.experienceInYears is not null)
             {
                 Jugador jugador = new(++lastId, jugadorDto.birthDate.GetValueOrDefault(), jugadorDto.name, jugadorDto.experienceInYears.GetValueOrDefault());
-                this.Jugadores.Add(jugador);
+                this.jugadores.Add(jugador);
+                _memoryCache.Set("lastId", this.lastId);
                 return jugador;
             }
             return null;
@@ -99,6 +123,7 @@ namespace PruebaAPI.Services
                     jugador.name = jugadorDto.name;
                 if (jugadorDto.experienceInYears.HasValue)
                     jugador.experienceInYears = jugadorDto.experienceInYears.GetValueOrDefault();
+                _memoryCache.Set("jugadores", this.jugadores);
                 return jugador;
             }
             return null;
@@ -108,7 +133,11 @@ namespace PruebaAPI.Services
         public void Eliminar(long id)
         {
             var jugador = this.GetById(id);
-            if (jugador is not null) this.Jugadores.Remove(jugador);
+            if (jugador is not null) 
+            { 
+                this.jugadores.Remove(jugador);
+                _memoryCache.Set("jugadores", this.jugadores);
+            }
         }
 
         //Metodos auxiliares
